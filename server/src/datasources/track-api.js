446ae4128441624507e6fdd8e8a9bbd5b376c5fa
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-11-29 17:29:10 +0300

    Update trackAPI

--- server/src/datasources/track-api.js
+++ server/src/datasources/track-api.js
@@ -15,6 +15,10 @@ class TrackAPI extends RESTDataSource {
     return this.get(`track/${trackId}`);
   }
 
+  getTrackModules(trackId) {
+    return this.get(`track/${trackId}/modules`);
+  }
+
   getAuthor(authorId) {
     return this.get(`author/${authorId}`);
   }
